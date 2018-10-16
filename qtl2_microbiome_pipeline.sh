7fdcde8d41a4e68e2818eab49059e58e51cb3d69
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-10-16 15:25:56 -0400

    Update and rename qtl_microbiome_pipeline.sh to qtl2_microbiome_pipeline.sh

---
+++ qtl2_microbiome_pipeline.sh
@@ -0,0 +1,24 @@
+### HPC shell script for qsubing qtl2_scan1_pomp_microbiome.R script
+#    Ran 1 at a time by commenting out the others and qsubing
+
+### Parameters after Rscript:
+#       1.) Name of R script file
+#       2.) Which taxa
+#       3.) Number of cores to run
+#       4.) Logical value: TRUE = qtl scan with rankZ transform data
+#                          FALSE = qtl scan with normalized data
+
+#PBS -l nodes=1:ppn=32
+#PBS -q batch
+#PBS -l walltime=72:00:00
+
+
+module load R/3.5.1
+
+
+Rscript qtl2_scan1_pomp_microbiome.R weinstock_16s_microbiome genus 32 TRUE
+Rscript qtl2_scan1_pomp_microbiome.R weinstock_16s_microbiome family 32 TRUE
+Rscript qtl2_scan1_pomp_microbiome.R weinstock_16s_microbiome class 32 TRUE
+Rscript qtl2_scan1_pomp_microbiome.R weinstock_16s_microbiome order 32 TRUE
+Rscript qtl2_scan1_pomp_microbiome.R weinstock_16s_microbiome phylum 32 TRUE
+
