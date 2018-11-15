42117f14d8415eb57721b0d36d2b85b2b3521685
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-11-15 13:47:02 -0500

    Add files via upload

---
+++ Sample Mix-Ups and Microbiome Mixture/Organize_Fastq/create_sample_directory.sh
@@ -0,0 +1,73 @@
+### Creating a directory for each sample
+#   There's a better way to do this...
+
+
+
+cd /projects/churchill-lab/data/Weinstock/Pomp_Benson/host_fastq/
+
+
+# Samples 401-417
+for i in {401..417};
+do
+    mkdir "MGS_DO_$i";
+done;
+
+
+
+# Samples 419-426
+for i in {419..426};
+do
+    mkdir "MGS_DO_$i";
+done; 
+
+
+
+# Samples 428-600
+for i in {428..600};
+do
+    mkdir "MGS_DO_$i";
+done;
+
+
+
+# Sample 675
+mkdir MGS_DO_675
+
+
+
+
+# Samples 678-682
+for i in {678..682};
+do
+    mkdir "MGS_DO_$i";
+done;
+
+
+
+# Samples 701-746
+for i in {701..746};
+do
+    mkdir "MGS_DO_$i";
+done;
+
+
+
+# Samples 801-838
+for i in {801..838};
+do
+    mkdir "MGS_DO_$i";
+done;
+
+
+
+
+# Samples 840-846
+for in {840..846};
+do
+    mkdir "MGS_DO_$i";
+done;
+
+
+
+# Sample 850
+mkdir MGS_DO_850
