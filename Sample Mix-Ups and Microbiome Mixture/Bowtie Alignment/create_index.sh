725b6b720208ed4ca6c33b661a1fe528deac82ef
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-11-19 15:37:49 -0500

    Update create_index.sh

--- Sample Mix-Ups and Microbiome Mixture/Bowtie Alignment/create_index.sh
+++ Sample Mix-Ups and Microbiome Mixture/Bowtie Alignment/create_index.sh
@@ -3,70 +3,24 @@
 module load bowtie2/2.3.1
 
 
+cd chromFA/
 
-
-
-
-
-### Directory where .fa genome and bowtie indexes will be stored
-mkdir mm10
-cd /home/phamd/mm10/
-DIR="/home/phamd/mm10/"
-
-
-
-
-
-
-
-
-
-
-
-
-### Create sub-directories for chromosomes 1-19 and X in the GRCm38_Index directory
+# Concatenate fasta files
+fasta_files=""
 for i in {1..19};
 do
-  mkdir "${DIR}Chromosome_$i";
-done;
+  fasta_files="${fasta_files},chr${i}.fa";
+done
 
-mkdir "${DIR}Chromosome_X"
-
-
+fasta_files="{$fasta_files},chrX.fa"
 
 
 
 
 
-
-
-### Moving .fa files to their corresponding directory
-cd /home/phamd/
-for i in {1..19};
-do
-  mv "chr${i}.fa" "${DIR}Chromosome_$i";
-done;
-mv chrX.fa "${DIR}Chromosome_X"
+echo $fasta_files
 
 
 
 
-
-
-
-
-
-### Create bowtie1 index for the autosome and X chromosome
-for i in {1..19};
-do
-  CHR_DIR="$DIR/Chromosome_$i"
-  cd ${CHR_DIR}
-  bowtie2-build "chr${i}.fa" "mm10_chr$i";
-  cd ${DIR}
-done;
-
-
-CHR_DIR="$DIR/Chromosome_X"
-cd ${CHR_DIR}
-bowtie2-build "chrX.fa" "mm10_chrX"
-
+bowtie2-build fasta_files
