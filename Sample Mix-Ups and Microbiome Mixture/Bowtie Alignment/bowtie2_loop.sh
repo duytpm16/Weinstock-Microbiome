01083319e55364fc10b25a2fd27ac780434be408
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-11-21 00:45:00 -0500

    Update bowtie2_loop.sh

--- Sample Mix-Ups and Microbiome Mixture/Bowtie Alignment/bowtie2_loop.sh
+++ Sample Mix-Ups and Microbiome Mixture/Bowtie Alignment/bowtie2_loop.sh
@@ -21,7 +21,12 @@ chromosome=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "1
 
 
 
+
+### Change to directory where fastq files are store for each sample
 cd $MGS_DIR
+
+
+
 
 
 
