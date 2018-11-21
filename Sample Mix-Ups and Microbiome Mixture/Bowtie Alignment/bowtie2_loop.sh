4a496dcab9e07c44ea58015f5fd8a56f37ed108d
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-11-20 20:02:58 -0500

    Add files via upload

---
+++ Sample Mix-Ups and Microbiome Mixture/Bowtie Alignment/bowtie2_loop.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+#PBS -l nodes=1:ppn=32
+#PBS batch
+
+module load bowtie2/2.3.1
+module load samtools/0.1.18
+
+
+
+
+
+
+
+### Variables to change
+# 1.) Directory where bwt index are stored
+# 2.) Directory where the fastq are stored
+# 3.) Array of chromosomes
+BWT_DIR="/home/phamd/chromFa/mm10.genome"
+MGS_DIR="/projects/churchill-lab/data/Weinstock/Pomp_Benson/host_fastq/"
+chromosome=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "X")
+cd $MGS_DIR
+
+
+
+
+
+
+
+
+
+### Loop through each of the MGS_DO_* directory
+for i in */;
+do
+
+  # Change to directory of the MGS_DO_* directory
+  sample=`echo -n $i | head -c -1`
+  SAMPLE_DIR="$MGS_DIR$i"
+  cd "$SAMPLE_DIR"
+  
+  echo $SAMPLE_DIR
+
+
+
+
+
+
+  ### Loop through each of the week sub-directory in the MGS_DO_* directory
+  for j in */;
+  do
+
+    # Save the week as a variable for bam outfile name
+    week=`echo -n $j | head -c -1`
+    
+
+    # Change to the Pair_End directory within the  week directory 
+    SAMPLE_WEEK_DIR="${SAMPLE_DIR}${j}"
+    SAMPLE_WEEK_PAIR_END_DIR="${SAMPLE_DIR}${j}Pair_End/"
+    SAMPLE_WEEK_SINGLETON_DIR="${SAMPLE_DIR}${j}Singletons/"    
+    
+
+
+    # Get the singleton reads
+    cd $SAMPLE_WEEK_SINGLETON_DIR
+    temp=`find . -iname '*b_host.fastq.3' | wc -l`
+    if [ $temp == 1 ]
+    then
+        singleton_1a=`ls | grep *a_host.fastq.3`
+        singleton_1b=`ls | grep *b_host.fastq.3`
+
+        singleton="${singleton_1a},${singleton_1b}"
+        singleton="${SAMPLE_WEEK_SINGLETON_DIR}${singleton}"
+    else
+
+        singleton=`ls | grep *fastq.3`
+        singleton="${SAMPLE_WEEK_SINGLETON_DIR}${singleton}"
+    fi
+
+
+
+    # Get the paired-end reads
+    cd "$SAMPLE_WEEK_PAIR_END_DIR"
+    temp=`find . -iname '*b_host.fastq*' | wc -l`
+    if [ $temp == 2 ]
+    then
+        pair_end_1a=`ls | grep *a_host.fastq.1`
+    	pair_end_2a=`ls | grep *a_host.fastq.2`
+        pair_end_1b=`ls | grep *b_host.fastq.1`
+    	pair_end_2b=`ls | grep *b_host.fastq.2`
+        
+	pair_end_1="${pair_end_1a},${pair_end_1b}"
+        pair_end_2="${pair_end_2a},${pair_end_2b}"
+    else
+
+        pair_end_1=`ls | grep *fastq.1`
+    	pair_end_2=`ls | grep *fastq.2`
+    fi
+
+
+    echo ${pair_end_1}
+    echo ${pair_end_2}
+    echo ${singleton}
+
+
+    ### Loop through each of the bwt indexes directory to perform bowtie alignment for every chromosome
+    bowtie2 -q -p 32 -x ${BWT_DIR} -1 ${pair_end_1} -2 ${pair_end_2} -U ${singleton} | samtools view -bS - | samtools sort - "${SAMPLE_WEEK_DIR}${sample}_${week}_sorted"
+    samtools index "${SAMPLE_WEEK_DIR}${sample}_${week}_sorted.bam"
+  cd ${SAMPLE_DIR}
+  done; # for j
+  
+  cd $MGS_DIR
+done; # for i
