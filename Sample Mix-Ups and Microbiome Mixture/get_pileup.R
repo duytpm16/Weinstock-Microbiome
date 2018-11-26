0077798e52ddd7c9ba1bdf292bb38fac9029eb41
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-11-26 14:48:49 -0500

    Update get_pileup.R

--- Sample Mix-Ups and Microbiome Mixture/get_pileup.R
+++ Sample Mix-Ups and Microbiome Mixture/get_pileup.R
@@ -4,32 +4,9 @@ library(qtl2)
 
 
 
-### Getting the CC variants of each chromosome and storing to a list
-query_variants <- create_variant_query_func("cc_variants.sqlite")
 
-snp_list <- list()
-for(i in c(1:19,'X')){
-  
-  
-    # Get CC variants at chromosome i
-    snp_info     <- query_variants(chr = i, start = 1, end = 1000)
-    snp_info$pos <- snp_info$pos * 1000000                           # Need to multiple pos by 1e6 to match with pileup position later in the script
-    
-    
-    
-    # Remove complex variants
-    complex  <- grepl("/", snp_info$alleles, fixed=TRUE)             
-    snp_info <- snp_info[!complex, ]
-    
-    
-    
-    snp_list[[i]] <- snp_info
-    
-    
-}
-
-saveRDS(snp_list, 'cc_variants_snp_list.rds')
-
+### Read in cc variants list as generated from get_cc_variants.R
+snp_list <- readRDS('cc_variants_snp_list.rds')
 
 
 
