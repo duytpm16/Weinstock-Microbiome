348122cb9f91d225ca2156ab84be6b6c566f4f0c
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-01 16:23:19 -0500

    Update get_pileup_parallel.R

--- Sample Mix-Ups and Microbiome Mixture/get_pileup_parallel.R
+++ Sample Mix-Ups and Microbiome Mixture/get_pileup_parallel.R
@@ -86,6 +86,7 @@ for(i in start:end){
            for(chromosome in c(1:19,'X')){
                load(paste0(snp_dir,'imputed_snps_chr_',chromosome,'.RData'))
                rm(list=c(paste0('imp_snps_chr',chromosome)))                                           # Need to remove imputed snp matrix other job will be killed due to large memory
+             
                snpinfo     <- get(paste0('snpinfo_chr',chromosome))
                snpinfo$pos <- round(snpinfo$pos * 1000000)
                
