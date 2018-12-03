85b686e8028d3bca605e5c0e3fcc168c6e6abdd4
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-03 14:21:57 -0500

    Update single_sample_result.R

--- Sample Mix-Ups and Microbiome Mixture/single_sample_result.R
+++ Sample Mix-Ups and Microbiome Mixture/single_sample_result.R
@@ -1,5 +1,15 @@
+### Options
+options(stringsAsFactors = FALSE)
 
-# get summaries for each MB sample against all DNA samples, one chr
+
+
+
+
+### Command line arguments / variables to change
+#     1.) home_directory : directory where my imputed snps .RData file are stored + where I want to save output of this script
+#     2.) fastq_directory: directory where each sub-directory is a DO sample and contains the fastq, pileup, and read counts
+#     3.) chr            : chromosome readcounts to get
+#     4.) week           : which week to get readcounts from
 args = commandArgs(trailingOnly = TRUE)
 home_directory  <- '/home/phamd/'
 fastq_directory <- "/projects/churchill-lab/data/Weinstock/Pomp_Benson/host_fastq/"
@@ -10,12 +20,19 @@ week            <- args[2]
 
 
 
-### load the corresponding imputed SNPs for that chromosome
+
+
+
+### Load the corresponding imputed SNPs and SNP info for that chromosome
 load(paste0("/home/phamd/imputed_snps_chr_", chr, ".RData"))
 imp_snps             <- get(paste0('imp_snps_chr', chr))                    # Get imputed snp matrix
 index_snpinfo        <- get(paste0('indexed_snpinfo_chr', chr))             # Get the indexed snp info dataframe
 index_snpinfo$pos_bp <- round(index_snpinfo$pos * 1e6)                      # Make new column of position in bp
 save_directory       <- paste0(home_directory,'week_',week,'/')             # Directory to store output
+
+
+
+
 
 
 
@@ -32,6 +49,8 @@ sample_id      <- paste0('DPDP.DO2.',sample_num,'.F')
 
 
 
+                         
+### Creating object to store results
 sample_results   <- vector("list", length(dir()))
 names(sample_results)   <- sample_id
 
@@ -41,12 +60,23 @@ names(sample_results)   <- sample_id
 
 
 
+                         
+                         
+                         
+                         
+                         
 ### Loop through each MGS_DO_* directory
 for(index in 1:length(sample_num)){
   
-    # Change to MGS_DO_* directory and week directory
+    # Get directory name
     sample <- paste0('MGS_DO_', sample_num[index])
     sample_week_directory <- paste0(fastq_directory, sample, '/Week_', week)
+  
+  
+  
+  
+  
+    # Change to MGS_DO_* directory and week directory if it exists
     if(dir.exists(sample_week_directory)){
        setwd(sample_week_directory)
        print(sample_week_directory)
@@ -54,13 +84,19 @@ for(index in 1:length(sample_num)){
   
   
   
-       # Read in read counts of one chromosome for one sample 
+      
+      
+      
+       # Read in read counts of one chromosome of one sample 
        sample_read_counts <- readRDS(paste0(sample,'_Week_',week,'_readcounts_chr_',chr,'.rds'))
        sample_read_counts <- sample_read_counts[!(sample_read_counts$Major_Count == 0 & sample_read_counts$Minor_Count == 0),] 
   
   
   
   
+      
+      
+      
   
        # Making sure all positions and snp id are the same
        aligning_pos           <- sample_read_counts$pos[sample_read_counts$pos %in% index_snpinfo$pos_bp]
@@ -70,28 +106,42 @@ for(index in 1:length(sample_num)){
        imp_snps_col           <- colnames(imp_snps)[colnames(imp_snps) %in% filtered_index_snpinfo$snp]
 
 
-
        print(length(imp_snps_col))
 
 
- 
+
+      
+      
+      
+      
+      
        # Create object to contain the results for the single samples
        sample_results[[index]] <- array(0, dim=c(nrow(imp_snps), 3, 2))
        dimnames(sample_results[[index]]) <- list(rownames(imp_snps), c("AA", "AB", "BB"), c("A", "B"))
    
   
-  
-  
-  
+ 
        for(i in 1:nrow(imp_snps)) {
            g <- imp_snps[i, imp_snps_col]
            for(j in 1:3) {
+            
                sample_results[[index]][i,j,1] <- sum(sample_read_counts$Major_Count[!is.na(g) & g==j])
                sample_results[[index]][i,j,2] <- sum(sample_read_counts$Minor_Count[!is.na(g) & g==j])
-           }
-       }
-    }
-}
+           } # for j
+       } # for i
+    
+      
+      
+      
+      
+    
+    } # If
+} # Loop through each MB sample
   
   
+                         
+                         
+                         
+                         
+# Save                         
 saveRDS(sample_results, file = paste0(save_directory, "sample_results_week_",week,"_chr_", chr, ".rds"))
