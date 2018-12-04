2213925ddfcb8b8c2b9aa7d6ecb310af84a0eacf
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-04 16:49:05 -0500

    Add files via upload

---
+++ Sample Mix-Ups and Microbiome Mixture/combine_results.R
@@ -0,0 +1,67 @@
+args = commandArgs(trailingOnly = TRUE)
+results_dir <- args[1]
+pattern     <- args[2]
+week        <- args[3]
+
+
+
+
+
+
+### Change to directory where results are stored
+setwd(results_dir)
+
+
+
+combined_results <- NULL
+
+
+
+for(chr in 1:19){
+    
+    # Get file name
+    results_file <- paste0(pattern, '_results_week_', week, '_chr_', chr, ".rds")
+    
+    print(results_file)
+    
+    # Check if file exists
+    if(file.exists(results_file)){
+      
+       # Read in file
+       result <- readRDS(results_file) 
+       
+       
+       
+       
+       
+       # If no results have been stored to combined_results, store result, else combine result with combined_results 
+       if(is.null(combined_results)){
+      
+          combined_results <- result
+          
+          
+          
+      
+       }else{
+          
+          stopifnot(length(result) == length(combined_results))
+         
+          result <- result[names(combined_results)]
+          
+          for(i in seq_along(combined_results)) {
+            
+              stopifnot(dim(result[[i]]) == dim(combined_results[[i]]),
+                        all(rownames(result[[i]]) == rownames(combined_results[[i]])))
+            
+            
+              combined_results[[i]] <- combined_results[[i]] + result[[i]]
+          }
+       }
+    }
+  
+
+}
+
+
+saveRDS(combined_results, file=paste0(pattern,"_results_all.rds"))
+
