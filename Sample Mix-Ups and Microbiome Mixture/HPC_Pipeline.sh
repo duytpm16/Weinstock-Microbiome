925c86bb98ad6a2a101286355c00e439d6f141c7
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-04 10:33:59 -0500

    Update HPC_Pipeline.sh

--- Sample Mix-Ups and Microbiome Mixture/HPC_Pipeline.sh
+++ Sample Mix-Ups and Microbiome Mixture/HPC_Pipeline.sh
@@ -3,18 +3,18 @@
 
 
 ### Parallel imputed snps for each chromosome
-#module load R/3.5.1
-#for i in {18..19}
-#do
-#echo "#PBS -q batch
-##PBS -l nodes=1:ppn=8
+module load R/3.5.1
+for i in {18..19}
+do
+echo "#PBS -q batch
+#PBS -l nodes=1:ppn=8
 
-#module load R/3.5.1
-#Rscript get_imputed_snps.R $i" >> get_imputed_snps_$i.sh
-#qsub get_imputed_snps_$i.sh
-#done
+module load R/3.5.1
+Rscript get_imputed_snps.R $i" >> get_imputed_snps_$i.sh
+qsub get_imputed_snps_$i.sh
+done
 
-#Rscript get_imputed_snps.R X
+Rscript get_imputed_snps.R X
 
 
 
@@ -23,48 +23,48 @@
 
 
 ### Parrallel get pileups for each sample
-#for((n=400; n<=855; n = n+5))
-#do
+for((n=400; n<=855; n = n+5))
+do
 
-#start=$(($n))
-#end=$(($n + 4))
-
-
-#echo "#PBS -q batch
-##PBS -l nodes=1:ppn=4
-##PBS -l walltime=24:00:00
-
-#module load R/3.5.1
-
-#Rscript get_pileup.R $start $end" >> get_pileup_$n.sh
-#qsub get_pileup_$n.sh
-
-#done
+start=$(($n))
+end=$(($n + 4))
 
 
+echo "#PBS -q batch
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=24:00:00
+
+module load R/3.5.1
+
+Rscript get_pileup.R $start $end" >> get_pileup_$n.sh
+qsub get_pileup_$n.sh
+
+done
 
 
 
 
 
-## Parrallel get readcounts for each sample
-#for((n=700; n<=705; n = n+5))
-#do
-
-#start=$(($n))
-#end=$(($n + 4))
 
 
-#echo "#PBS -q batch
-##PBS -l nodes=1:ppn=4
-##PBS -l walltime=24:00:00
+# Parrallel get readcounts for each sample
+for((n=400; n<=850; n = n+5))
+do
 
-#module load R/3.5.1
-#
-#Rscript get_readcounts.R $start $end" >> get_readcounts_$n.sh
-#qsub get_readcounts_$n.sh
+start=$(($n))
+end=$(($n + 4))
 
-#done
+
+echo "#PBS -q batch
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=24:00:00
+
+module load R/3.5.1
+
+Rscript get_readcounts.R $start $end" >> get_readcounts_$n.sh
+qsub get_readcounts_$n.sh
+
+done
 
 
 
@@ -75,18 +75,18 @@
 ### Parallel imputed snps for each chromosome
 week=("6" "17" "24")
 chromosome=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "X")
-#for w in "${week[@]}"
-#do
-#  for i in "${chromosome[@]}"
-#  do
-#    echo "#PBS -q batch
-#    #PBS -l nodes=1:ppn=4
+for w in "${week[@]}"
+do
+  for i in "${chromosome[@]}"
+  do
+    echo "#PBS -q batch
+    #PBS -l nodes=1:ppn=4
 
-#    module load R/3.5.1
-#    Rscript single_sample_result.R $i $w" >> "sample_results_wk${w}_${i}.sh"
-#    qsub "sample_results_wk${w}_${i}.sh"
-#  done
-#done
+    module load R/3.5.1
+    Rscript single_sample_result.R $i $w" >> "sample_results_wk${w}_${i}.sh"
+    qsub "sample_results_wk${w}_${i}.sh"
+  done
+done
 
 
 
