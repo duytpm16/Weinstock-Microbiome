9f532ac038896d235c25dd7dd5abd35c66869ff6
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-14 13:36:18 -0500

    Update mbmixture_analysis.R

--- Sample Mix-Ups and Microbiome Mixture/mbmixture_analysis.R
+++ Sample Mix-Ups and Microbiome Mixture/mbmixture_analysis.R
@@ -1,3 +1,4 @@
+### Options and Libraries
 options(stringsAsFactors = FALSE)
 library(dplyr)
 library(mbmixture)
@@ -5,12 +6,23 @@ library(parallel)
 
 
 
+
+
+### Read in required data
+#     1.) pair:  pair result list
+#     2.) week:  which week
+#     3.) cores: how many cores
 args  <- commandArgs(trailingOnly = TRUE)
 pair  <- readRDS(args[1])
 week  <- args[2]
 cores <- as.numeric(args[3])
 
 
+
+
+
+
+### Do MB mixture analysis for each sample in pair
 analyze_one <- function(i) {
   res <- matrix(nrow=nrow(pair[[i]]), ncol=5)
   
@@ -24,11 +36,18 @@ analyze_one <- function(i) {
 }
 
 
+
+
+
+
 result <- mclapply(seq_along(pair), analyze_one, mc.cores=cores)
 names(result) <- names(pair)
 
 
 
 
+
+
+### Save results
 saveRDS(result, file=paste0("mixture_results_wk_",week,".rds"))
 
