927c01f72037c742682ca31c046c20e1fddbea30
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-01-01 18:34:15 -0500

    Update single_proportion_discordant.R

--- Sample Mix-Ups and Microbiome Mixture/single_proportion_discordant.R
+++ Sample Mix-Ups and Microbiome Mixture/single_proportion_discordant.R
@@ -18,4 +18,11 @@ f <- function(a, rn ) {
 samp_p_w6 <- t(sapply(single_results_wk6_all_v2, f, rownames(single_results_wk6_all_v2[[1]])))
 samp_p_w17 <- t(sapply(single_results_wk17_all_v2, f, rownames(single_results_wk17_all_v2[[1]])))
 samp_p_w24 <- t(sapply(single_results_wk24_all_v2, f, rownames(single_results_wk24_all_v2[[1]])))
-saveRDS(samp_p, file="single_results.rds")
+             
+             
+             
+             
+             
+saveRDS(samp_p_w6,  file="single_proportion_discordant_results_w6_v2.rds")
+saveRDS(samp_p_w17, file="single_proportion_discordant_results_w17_v2.rds")
+saveRDS(samp_p_w24, file="single_proportion_discordant_results_w24_v2.rds")
