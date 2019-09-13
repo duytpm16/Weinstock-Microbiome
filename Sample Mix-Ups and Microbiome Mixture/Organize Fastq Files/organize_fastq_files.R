dab0afdf8cd09b7353d236909da6db11a3826b4d
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-09-13 15:05:43 -0400

    Update and rename organize_fastq.R to organize_fastq_files.R

--- Sample Mix-Ups and Microbiome Mixture/Organize Fastq Files/organize_fastq_files.R
+++ Sample Mix-Ups and Microbiome Mixture/Organize Fastq Files/organize_fastq_files.R
@@ -34,8 +34,9 @@ setwd(orig_dir)
 
 
 
-### Within each of the sample's directory, create a directory for week 6, 17, and 24, and then within each of the week's
-#       directory, create a Pair_End and Singleton directory. Move files to their appropriate directory.
+### Within each of the sample's directory, create a directory for week 6, 17, and 24.
+#     Then within each of the week's directory, create a Pair_End and Singleton directory. 
+#     Finally move files to their appropriate directory.
 for(i in dir()){
 
 
@@ -57,11 +58,11 @@ for(i in dir()){
     
     
     # Moving files to their corresponding week directory
-    week_6_mwgs <- dir()[grep('*-w6-*',dir())]
+    week_6_mwgs  <- dir()[grep('*-w6-*',dir())]
     week_17_mwgs <- dir()[grep('*-w17-*',dir())]    
     week_24_mwgs <- dir()[grep('*-w24-*',dir())]
     
-    sapply(week_6_mwgs, FUN = function(x) file.move(x, paste0(mwgs_directory,'Week_6/')))
+    sapply(week_6_mwgs,  FUN = function(x) file.move(x, paste0(mwgs_directory,'Week_6/')))
     sapply(week_17_mwgs, FUN = function(x) file.move(x, paste0(mwgs_directory,'Week_17/'))) 
     sapply(week_24_mwgs, FUN = function(x) file.move(x, paste0(mwgs_directory,'Week_24/')))
 
@@ -70,14 +71,13 @@ for(i in dir()){
 
 
 
-    ### For each of the week directory, create a Pair_End and Singleton directory and move files to their corresponding directory
+   
 
-    #   According to Jethro:
-    #      fastq.1 and fastq.2 files are pair-end reads. Corresponding reads appear at the same position in each file
-    #      fastq.3 are singleton reads whose mate pair was lost during trimming
+    # According to Jethro:
+    #    fastq.1 and fastq.2 files are pair-end reads. Corresponding reads appear at the same position in each file
+    #    fastq.3 are singleton reads whose mate pair was lost during trimming
 
-
-    #   Do above for week 6 fastq files
+    # For each of the week directory, create a Pair_End and Singleton directory and move files to their corresponding directory
     setwd(paste0(mwgs_directory,'/Week_6/'))                                                # Change directory to the week 6 directory
  
     dir.create(paste0(getwd(),'/Pair_End/'))                                                # Create Pair_End directory
@@ -91,10 +91,13 @@ for(i in dir()){
  
     setwd(mwgs_directory)                                                                   # Move back to the MGS_DO_* directory
      
-       	      										    # Above lines are repeated for week 17 and 24
+       	      			 # Above lines are repeated for week 17 and 24
     
  
 
+           
+           
+           
     #   Do above for week 17 fastq files
     setwd(paste0(mwgs_directory,'/Week_17/'))
     
@@ -111,6 +114,7 @@ for(i in dir()){
 
     
 
+           
 
 
     #  Do above for week 24 fastq files
