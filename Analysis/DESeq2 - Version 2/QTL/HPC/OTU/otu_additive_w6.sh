087b9fc9d344ca0e8f7683bf629e9f34aa17a0eb
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-09-23 14:49:40 -0400

    Update and rename otu_w6_1.sh to otu_additive_w6.sh

--- Analysis/DESeq2 - Version 2/QTL/HPC/OTU/otu_additive_w6.sh
+++ Analysis/DESeq2 - Version 2/QTL/HPC/OTU/otu_additive_w6.sh
@@ -1,5 +1,11 @@
+### Shell script to run qtl2 on HPC with data in QTL viewer format.
+#   This is for additive scan (int_name = 'NA').
+#   I did not run this in chunks, hence the 'NA'.
+#   See https://github.com/duytpm16/qtl2-HPC-pipeline for input details.
+
+
 #PBS -q batch
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=8
 #PBS -l walltime=24:00:00
 
 module load R/3.5.1
