070fb908b95d8b99cdcd9c5d1793676990c36e34
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-09-23 14:53:54 -0400

    Add files via upload

---
+++ Analysis/DESeq2 - Version 2/QTL/HPC/Class/class_w6_1.sh
@@ -0,0 +1,16 @@
+#PBS -q batch
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=24:00:00
+
+module load R/3.5.1
+
+viewer_data='weinstock_pomp_16s_microbiome_qtl_viewer_v4.RData'
+dataset_expr='dataset.class.w6|data|rz'
+num_cores='1'
+int_name='NA'
+chunk_number='NA'
+chunk_size='NA'
+
+
+
+Rscript qtl2_scan1.R viewer_data=$viewer_data dataset_expr=$dataset_expr num_cores=$num_cores int_name=$int_name chunk_number=$chunk_number chunk_size=$chunk_size
