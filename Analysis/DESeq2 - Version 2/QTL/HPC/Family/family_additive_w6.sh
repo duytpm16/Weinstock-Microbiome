3d08f90bc38aae1cf23ff9156e1e62dd52d14462
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-09-23 14:53:11 -0400

    Update family_additive_w6.sh

--- Analysis/DESeq2 - Version 2/QTL/HPC/Family/family_additive_w6.sh
+++ Analysis/DESeq2 - Version 2/QTL/HPC/Family/family_additive_w6.sh
@@ -5,14 +5,14 @@
 
 
 #PBS -q batch
-#PBS -l nodes=1:ppn=2
+#PBS -l nodes=1:ppn=8
 #PBS -l walltime=24:00:00
 
 module load R/3.5.1
 
 viewer_data='weinstock_pomp_16s_microbiome_qtl_viewer_v4.RData'
 dataset_expr='dataset.family.w6|data|rz'
-num_cores='2'
+num_cores='8'
 int_name='NA'
 chunk_number='NA'
 chunk_size='NA'
