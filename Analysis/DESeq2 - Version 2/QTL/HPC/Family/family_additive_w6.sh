86ff7ead0b49cc0b3148a9f9b58a49f241c6b3c2
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-09-23 14:52:33 -0400

    Update and rename family_w6_1.sh to family_additive_w6.sh

--- Analysis/DESeq2 - Version 2/QTL/HPC/Family/family_additive_w6.sh
+++ Analysis/DESeq2 - Version 2/QTL/HPC/Family/family_additive_w6.sh
@@ -1,3 +1,9 @@
+### Shell script to run qtl2 on HPC with data in QTL viewer format.
+#   This is for additive scan (int_name = 'NA').
+#   I did not run this in chunks, hence the 'NA'.
+#   See https://github.com/duytpm16/qtl2-HPC-pipeline for input details.
+
+
 #PBS -q batch
 #PBS -l nodes=1:ppn=2
 #PBS -l walltime=24:00:00
