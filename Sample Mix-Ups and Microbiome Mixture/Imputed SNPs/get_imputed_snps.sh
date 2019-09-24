987015dfe317f29c2a908e69f8290288e35771d6
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-09-24 16:37:29 -0400

    Update get_imputed_snps.sh

--- Sample Mix-Ups and Microbiome Mixture/Imputed SNPs/get_imputed_snps.sh
+++ Sample Mix-Ups and Microbiome Mixture/Imputed SNPs/get_imputed_snps.sh
@@ -1,11 +1,7 @@
-
-
-
 for i in {1..19};
 do
 
-  echo "
-  #PBS -l nodes=1:ppn=4
+  echo " #PBS -l nodes=1:ppn=4
   module load R/3.5.1
 
   Rscript get_imputed_snps.R ${i} 4" >> get_imputed_snps_chr_$i.sh
